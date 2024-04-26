9a001da9d81deebf9b17bd92acf4a725983c2d07
Gutierrez Angel Gonzalo <chromaticsadboy@gmail.com>
2024-04-26 15:41:13 -0300

    Add exercise 4

---
+++ tp2_GutierrezAngelGonzalo/src/ar/edu/unju/fi/exercise4/constant/Position.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.exercise4.constant;
+
+/**
+ * @author Gutierrez Angel Gonzalo
+ * @version 1.0
+ */
+public enum Position {
+    DELANTERO,
+    MEDIO,
+    DEFENSA,
+    ARQUERO;
+}
